736c6deeda160aede9d8e2c43391586d50e6e4b1
Daniel <danielcanter2013@gmail.com>
2025-05-11 18:28:36 +0300

    messages.go added

---
+++ cmd/bot/messages/messages.go
@@ -0,0 +1,42 @@
+package messages
+
+const BILLY_TEAR_OFF_VEST = "CAACAgIAAxkBAAEIcRNkKxD1kytU2onQeZaO_WDgSaUV2gACAhgAAnLcwEt56Ty4vsWYDC8E"
+const DRINK_CHAMPAGNE = "CAACAgIAAxkBAAEIdW5kLF5B8ohszTGZmCfZc4bFGFs-jgACdhIAAszdyUv8JECqGA168S8E"
+
+
+
+var PIDOR_MESSAGES = [...]string{
+	"Aflu cine e cel mai mare FAFlist",
+    "Scanez cine nu face domashka la bostan 😏",
+    "Cine cauta hamiltonian cycle 🤡",
+    "Cautam cine are cana cu Gaidau 🌈",
+    "Analizam cine o pizdit carnea de la xFAF 🥓",
+    "Cine asculta la lectiile lui Gogoi? 💅🏾",
+    "Caut cine va sterge tabla la Cojuhari👀",
+    "Cautam urodu in familie 🙈",
+} 
+
+var NICE_MESSAGES = [...]string {
+    "Cautam cine scrie codul singur 🦾",
+    "Pravireaiem cine are ChatGPT Pro 🧑‍💻✨",
+    "Verificam la cine a padcatit Gaidau 🚀",
+    "Caut cui D. Artur i-a scos 12 pe simestru 🎯",
+    "Vad cine mananca zilnic la Das Kebab 🏆",
+    "Cine mananca pizzdeala 🧪👑",
+    "Blea 🍕❤️",
+}
+
+const (
+	PIDOR_1_TIME = "1 time - kebab kebab 😅"
+	PIDOR_10_TIME = "10 times - kebab master plus plus 🤔"
+	PIDOR_50_TIME = "50 times - turkish kebab suleiman pro macs 🛠️"
+	PIDOR_100_TIME = "100 times - bostan kebab auf hui musaram 💼🔥"
+)
+
+const (
+	NICE_1_TIME = "1 time - LOJ! 🍀"
+	NICE_10_TIME = "10 times - Almost fistic 👼"
+	NICE_50_TIME = "50 times - Almost Dominic ⭐️"
+	NICE_100_TIME = "100 times - Esti neam cu bostan? 💾🚀"
+)
+
